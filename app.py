96c4cf0371db4dc1c91ba5f1483d1e616b825937
Antoine <avollet@gaming.tech>
2024-05-02 15:48:42 +0200

    ta race la grosse pute

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
 
-from constants import *
-
 import matplotlib.pyplot as plt
-
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from hermite import DrawPlot
 
+from hermite import DrawPlot
+from hermite_params import HermiteParams
+from constants import *
 
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -17,6 +16,7 @@ class App(tk.Tk):
         
         self.initWindow()
 
+        self.hermiteParams = HermiteParams()
         self.fig, self.ax = DrawPlot(points, slopes, precision, True, False, False, centerPoint, intervalLength, True)
 
     def initWindow(self):
@@ -45,18 +45,22 @@ class App(tk.Tk):
     def draw(self):
         self.config(bg=C_SEPARATION_LINE)
 
-        self.sidebarCanvas = tk.Canvas(self, width=SIDEBAR_WIDTH, bg=C_SECONDARY, bd=0, highlightthickness=0)
-        self.sidebarCanvas.pack(side="left", fill="y")
-        self.sidebarCanvas.pack_propagate(0)
+        self.sidebarFrame = tk.Frame(self, width=SIDEBAR_WIDTH, bg=C_SECONDARY, bd=0, highlightthickness=0)
+        self.sidebarFrame.pack(side="left", fill="y")
+        self.sidebarFrame.pack_propagate(0)
 
-        self.renderCanvas = tk.Canvas(self, width=DEFAULT_WIDTH - SIDEBAR_WIDTH - SEPARATION_LINE_WIDTH, bg=C_MAIN,
+        self.hermiteParams.draw(self.sidebarFrame)
+
+        self.renderFrame = tk.Frame(self, width=DEFAULT_WIDTH - SIDEBAR_WIDTH - SEPARATION_LINE_WIDTH, bg=C_MAIN,
             bd=0, highlightthickness=0)
-        self.renderCanvas.pack(side="right", fill="y")
-        self.renderCanvas.pack_propagate(0)
-        
+        self.renderFrame.pack(side="right", fill="y")
+        self.renderFrame.pack_propagate(0)
+
+        self.add_button = tk.Button(self.renderFrame, bg="green", text='+', width=50, height=50)
+        self.add_button.pack(padx=0, pady=0, ipadx=50, ipady=50)
         self.fig.set_facecolor("none")
         self.ax.set_facecolor("none")
-        pltCanvas = FigureCanvasTkAgg(self.fig, master=self.renderCanvas)
+        pltCanvas = FigureCanvasTkAgg(self.fig, master=self.renderFrame)
         self.pltWidget = pltCanvas.get_tk_widget()
 
         figW, figH = self.fig.get_size_inches()
@@ -64,6 +68,6 @@ class App(tk.Tk):
         self.pltWidget.configure(bg=C_MAIN, width=(1920-SIDEBAR_WIDTH)*0.8, height=(1920-SIDEBAR_WIDTH) * ratio*0.8)
         self.pltWidget.pack()
 
-        self.versionLabel = tk.Label(self.renderCanvas, text='Version {}'.format(CURRENT_VERSION), fg=C_TEXT, bg=C_MAIN,
+        self.versionLabel = tk.Label(self.renderFrame, text='Version {}'.format(CURRENT_VERSION), fg=C_TEXT, bg=C_MAIN,
             font=F_CALIBRI)
         self.versionLabel.place(rely=1.0, relx=1.0, x=0, y=0, anchor="se")
--- hermite_params.py
+++ hermite_params.py
@@ -1,8 +1,25 @@
 import tkinter as tk
+from tkinter import ttk
+
+from constants import *
 
 class HermiteParams:
-    def __init__(self, master: tk.Canvas=None):
-        self.master = master
+    def __init__(self):
+        self.points = []
 
-    def draw(self):
-        self.master.grid_rowconfigure
+        self.binButtonImg = tk.PhotoImage(file="res/images/bin_button.png")
+
+    def draw(self, master: tk.Frame=None):
+        self.pointsFrame = tk.Frame(master, bg="red", height=50)
+        self.pointsFrame.pack(fill="x", padx=30, pady=10)
+        self.pointsFrame.pack_propagate(0)
+
+        # self.addPointButton = tk.Button(self.pointsFrame, image=tk.PhotoImage(file=r"C:/Users/avollet/Documents/Hermite_Mathematiques/res/images/add_button.png"))
+        # self.addPointButton.pack(side="right")
+
+        self.deleteButton = tk.Button(self.pointsFrame, width=1, height=1, bg=C_SECONDARY, image=self.binButtonImg)
+        self.deleteButton.pack(side="right")
+
+        self.pointsCombobox = ttk.Combobox(self.pointsFrame, height=40, background=C_MAIN, foreground=C_TEXT, values=["a", "b", "c"], font=F_CALIBRI)
+        self.pointsCombobox.current(0)
+        self.pointsCombobox.pack(fill='x')
